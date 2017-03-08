2a6e75e1185ce03d7c91c707cb525dee75b861e4
tanujv <tanujv@tanujv.vakrangee.com>
2017-03-08 13:45:29 +0530

    1

--- src/main/java/com/solutionmafia/ethereumpoc/main/MainApp.java
+++ src/main/java/com/solutionmafia/ethereumpoc/main/MainApp.java
@@ -1,9 +1,15 @@
 package com.solutionmafia.ethereumpoc.main;
 
 import com.solutionmafia.ethereumpoc.utils.Web3JUtils;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import org.apache.log4j.Logger;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
@@ -20,13 +26,27 @@ public class MainApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        log.info("Welcome to Ethereum POC!");
+        String version = null;
         try {
-            log.info("Welcome to Ethereum POC!");
             Web3j web3 = Web3j.build(new HttpService());
-            String version = Web3JUtils.getVersion(web3);
-            log.info("version: " + version);
+            version = Web3JUtils.getVersion(web3);
         } catch (InterruptedException | ExecutionException ex) {
             log.error(ex);
+        }
+        log.info("version: " + version);
+        if (version != null) {
+            try {
+                ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+                BigInteger privateKey = ecKeyPair.getPrivateKey();
+                BigInteger publicKey = ecKeyPair.getPublicKey();
+                log.info("privateKey: " + privateKey);
+                log.info("publicKey: " + publicKey);
+            } catch (InvalidAlgorithmParameterException | NoSuchAlgorithmException | NoSuchProviderException ex) {
+                log.error(ex);
+            }
+
         }
     }
 
